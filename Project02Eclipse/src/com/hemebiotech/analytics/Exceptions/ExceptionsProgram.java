e4aaec39f3d7c3faa4c0a20b25afbb9d41b77029
Nico-GS <ng73800@outlook.fr>
2020-07-02 13:13:02 +0200

    Class d'exceptions

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Exceptions/ExceptionsProgram.java
@@ -0,0 +1,12 @@
+package com.hemebiotech.analytics.Exceptions;
+
+public class ExceptionsProgram extends Exception{
+
+    // Exceptions
+    public ExceptionsProgram (String s){
+        super(s);
+    }
+
+
+
+}
