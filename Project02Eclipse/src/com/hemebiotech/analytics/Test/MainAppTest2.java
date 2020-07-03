93ab59d14e650a67b9d73d248728c17c5bcbcdbd
Nico-GS <ng73800@outlook.fr>
2020-07-03 16:04:17 +0200

    MainAppTest2 = Programme de test avec TreeMap

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Test/MainAppTest2.java
@@ -0,0 +1,37 @@
+package com.hemebiotech.analytics.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class MainAppTest2 {
+
+    public static void main (String[] args) {
+
+        try {
+            File file = new File("Project02Eclipse\\symptoms.txt");
+            Scanner scan = new Scanner(file);
+
+            Map<String, Integer> wordCount = new TreeMap<>();
+            while(scan.hasNext()) {
+                String word = scan.next();
+                if(!wordCount.containsKey(word))
+                    wordCount.put(word, 1);
+                else
+                    wordCount.put(word, wordCount.get(word) + 1);
+            }
+
+            // Resultats
+            for(String word : wordCount.keySet())
+                System.out.println(word + " " + wordCount.get(word));
+            System.out.println(wordCount.size());
+        }
+        catch(IOException e) {
+            System.out.println("Fichier introuvable");
+        }
+
+    }
+}
