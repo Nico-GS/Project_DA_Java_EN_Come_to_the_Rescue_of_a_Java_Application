5d35539b0415849d627147fd1bb747d2233a86ad
Nico-GS <ng73800@outlook.fr>
2020-07-03 16:31:05 +0200

    MainAppTest2 = Programme de test avec TreeMap

--- Project02Eclipse/src/com/hemebiotech/analytics/Test/MainAppTest2.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Test/MainAppTest2.java
@@ -1,35 +1,39 @@
 package com.hemebiotech.analytics.Test;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class MainAppTest2 {
 
     public static void main (String[] args) {
 
         try {
-            File file = new File("Project02Eclipse\\symptoms.txt");
-            Scanner scan = new Scanner(file);
+            File file = new File ("Project02Eclipse\\symptoms.txt");
+            Scanner scan = new Scanner (file);
 
-            Map<String, Integer> wordCount = new TreeMap<>();
-            while(scan.hasNext()) {
-                String word = scan.next();
-                if(!wordCount.containsKey(word))
-                    wordCount.put(word, 1);
+            Map<String, Integer> wordCount = new TreeMap<> ();
+            while (scan.hasNext ()) {
+                String word = scan.next ();
+                if (!wordCount.containsKey (word))
+                    wordCount.put (word, 1);
                 else
-                    wordCount.put(word, wordCount.get(word) + 1);
+                    wordCount.put (word, wordCount.get (word) + 1);
             }
 
-            // Resultats
-            for(String word : wordCount.keySet())
-                System.out.println(word + " " + wordCount.get(word));
-            System.out.println(wordCount.size());
-        }
-        catch(IOException e) {
-            System.out.println("Fichier introuvable");
+            // Result in console & Write file output
+            FileWriter writer = new FileWriter ("resultat2.out");
+            BufferedWriter out = new BufferedWriter (writer);
+            for (Map.Entry<String, Integer> entry : wordCount.entrySet ()){
+                System.out.println ("Valeur : " + entry.getKey () + "| Occurence : " + entry.getValue ());
+                out.write (entry.getKey () + " " +  entry.getValue () + " \n");
+                out.flush ();
+            }
+
+        } catch (IOException e) {
+            System.out.println ("Fichier introuvable");
         }
 
     }
