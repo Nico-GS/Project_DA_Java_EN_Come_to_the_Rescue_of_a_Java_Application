0ba21c8975d3df362983a375c67ac0a5b97a6049
Nico-GS <ng73800@outlook.fr>
2020-07-13 15:39:00 +0200

    Ajout class FileError & WriteError

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/packages/exceptions/FileError.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics.packages.exceptions;
+
+public class FileError extends Exception {
+
+    public FileError (String s) {
+        super(s);
+    }
+
+}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/packages/exceptions/WriteError.java
@@ -0,0 +1,10 @@
+package com.hemebiotech.analytics.packages.exceptions;
+
+public class WriteError extends Exception {
+
+    public WriteError (String s) {
+        super (s);
+    }
+
+}
+
