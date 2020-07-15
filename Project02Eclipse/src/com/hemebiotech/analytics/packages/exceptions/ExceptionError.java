bc1030e2926654ec6eb34f9776602798a5a9f7cc
Nico-GS <ng73800@outlook.fr>
2020-07-15 14:18:51 +0200

    Modif gestion des exceptions

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/packages/exceptions/ExceptionError.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics.packages.exceptions;
+
+public class ExceptionError extends Exception {
+
+    public ExceptionError () {
+    }
+
+    public ExceptionError (String message, Throwable cause) {
+        super (message, cause);
+    }
+
+    public ExceptionError (Throwable cause) {
+        super (cause);
+    }
+
+
+}
