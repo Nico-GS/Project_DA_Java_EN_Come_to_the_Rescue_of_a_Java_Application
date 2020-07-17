49e95a0df6e49f528368f86a61e044765bdd4d1d
Nico-GS <ng73800@outlook.fr>
2020-07-17 14:40:13 +0200

    Modif gestion des exceptions + corrections & améliorations

--- Project02Eclipse/src/com/hemebiotech/analytics/packages/exceptions/WrongPath.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/packages/exceptions/WrongPath.java
@@ -2,8 +2,6 @@ package com.hemebiotech.analytics.packages.exceptions;
 
 public class WrongPath extends ExceptionError {
 
-    // renommer
-
     public WrongPath () {
         super ();
         System.out.println ("Erreur lecture fichier : vérifier le chemin");
