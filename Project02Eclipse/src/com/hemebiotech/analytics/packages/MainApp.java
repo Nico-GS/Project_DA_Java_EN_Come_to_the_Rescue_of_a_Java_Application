7926a602eb76f0869950a09e522d2f28fa2d35bb
Nico-GS <ng73800@outlook.fr>
2020-07-29 14:33:27 +0200

    MainApp : Exécution de l'application

--- Project02Eclipse/src/com/hemebiotech/analytics/packages/MainApp.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/packages/MainApp.java
@@ -7,6 +7,7 @@ public class MainApp {
 
     public static void main (String[] args) throws ExceptionError {
 
+        // Launch the app from BiotechController
         BiotechController app = new BiotechController ();
         app.run ();
     }
