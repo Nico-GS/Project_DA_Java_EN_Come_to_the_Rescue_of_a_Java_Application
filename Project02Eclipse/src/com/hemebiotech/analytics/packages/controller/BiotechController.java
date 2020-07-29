aa1d5db3b6e5d4be677a29211a45b50d502a30aa
Nico-GS <ng73800@outlook.fr>
2020-07-29 14:41:08 +0200

    Controlleur de l'application, exécute les différentes étapes

--- Project02Eclipse/src/com/hemebiotech/analytics/packages/controller/BiotechController.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/packages/controller/BiotechController.java
@@ -13,15 +13,25 @@ import java.util.Map;
 
 public class BiotechController {
 
+    /**
+     *
+     * Select the path, create a TreeMap and write a file
+     *
+     * @throws ExceptionError the exception error
+     */
     public void run () throws ExceptionError {
 
+        // Select external file
         ISymptomReader symptomFromFile = new ReadSymptomDataFromFile ("Project02Eclipse\\symptoms.txt");
         List<String> listSymptom = new ArrayList<> (symptomFromFile.getSymptoms ());
 
+        // Create TreeMap of the external file
         SymptomMap symptomToMap = new SymptomMap ();
         Map<String, Long> mapSymptom = symptomToMap.toMap (listSymptom);
+        // Writing console result
         System.out.println (symptomToMap.toMap (listSymptom));
 
+        // Write output file
         ISymptomWriter write = new WriteFile ("results.out");
         write.write (mapSymptom);
     }
